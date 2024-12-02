f82e59e16ad6e72100e54d776df07dd26edf80de
Sandeep <urmsandeep@gmail.com>
2024-12-02 21:24:04 +0000

    Updated Jenkinsfile

--- test_app.py
+++ test_app.py
@@ -5,6 +5,7 @@ class TestApp(unittest.TestCase):
     def test_home(self):
         tester = app.test_client()
         response = tester.get("/")
+        print(response.data.decode("utf-8"))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data.decode("utf-8"), "Hello, Jenkins Multi-Stage Pipeline!")
 
